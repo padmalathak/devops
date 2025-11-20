d51a1e10d0526a230ddab2b46c1e22e26ce8a6f0
Padma <padmalathatce@gmail.com>
2025-11-20 16:26:34 -0600

    feature/Add ci pipelines

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello Devops");
+console.log("DevOps index file loaded successfully.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting DevOps tests...");
+setTimeout(() => {
+  console.log("DevOps tests completed successfully. 3secs");
+}, 3000);
+console.log("DevOps test script is done.");
